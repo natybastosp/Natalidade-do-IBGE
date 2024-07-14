5b44564ec9e0f465cfac3c01afe3c8c9f7394b78
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2024-07-14 20:18:50 -0300

    Adiciona a biblioteca tailwindcss css

--- natalidade/src/App.js
+++ natalidade/src/App.js
@@ -1,13 +1,21 @@
-import "./App.css";
+import React from "react";
+import "./index.css";
 
 function App() {
   return (
     <div>
-      <header className="SideBar">
-        <div>
-          <h1>Nome de cidade</h1>
-        </div>
+      <header className="bg-blue-500 text-white p-4 h-screen w-56 fixed left-0 top-0">
+        <h1 className="text-2xl">Nataliada Br</h1>
       </header>
+      <main className="p-4 ml-56">
+        <div className="grid grid-cols-3 gap-8 h-16 ">
+          <div className="bg-black p-4 rounded-lg">
+            <p> Taxa de nataliada</p>
+          </div>
+          <div className="bg-black p-4 rounded-lg ">box de dados</div>
+          <div className="bg-black p-4 rounded-lg ">box de dados</div>
+        </div>
+      </main>
     </div>
   );
 }
