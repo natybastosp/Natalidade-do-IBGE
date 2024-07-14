3585e2421b7912483181ca83a215ba7a1b363494
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2024-07-14 17:57:57 -0300

    Ajuste de pasta

---
+++ natalidade/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="SideBar">
+        <div>
+          <h1>Nome de cidade</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
