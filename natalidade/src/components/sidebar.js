f6610a0e238d601de3f0a60e5a2a7953de4970af
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2024-07-14 21:18:37 -0300

    Cria o componente de `Sidebar`

---
+++ natalidade/src/components/sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div>
+      <div className="bg-blue-500 text-white p-4 h-screen w-56 fixed left-0 top-0">
+        <h1 className="text-2xl">Nataliada Br</h1>
+        <p>Home</p>
+        <p>Dashboard</p>
+        <p>Atulalizar dados</p>
+
+        <p>Estado</p>
+        <p>hora e data do get</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
