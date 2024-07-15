531332aef0d46c30bf41a9ab1366f88528e7962d
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2024-07-14 21:17:55 -0300

    Cria o componente de `Card`

---
+++ natalidade/src/components/card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <div className="bg-zinc-400 p-4 rounded-lg h-20 ">box de dados</div>
+    </div>
+  );
+}
+
+export default Card;
